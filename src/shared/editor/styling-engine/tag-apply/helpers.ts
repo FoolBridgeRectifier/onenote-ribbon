d3cc7c96adc3c49debe00fb7c5bc5398148c3669
Dragus <zathyadark@gmail.com>
2026-04-21 05:07:30 -0500

    feat: add removeCalloutByKey for targeted nested callout removal

--- src/shared/editor/styling-engine/tag-apply/helpers.ts
+++ src/shared/editor/styling-engine/tag-apply/helpers.ts
@@ -2,3 +2,4 @@ export { detectActiveTagKeys } from './helpers/detect-active-tag-keys/DetectActi
 export { removeActiveCallout } from './helpers/remove-active-callout/RemoveActiveCallout';
 export { removeActiveCheckbox } from './helpers/remove-active-checkbox/RemoveActiveCheckbox';
 export { toggleInlineTodoTag } from './helpers/toggle-inline-todo-tag/ToggleInlineTodoTag';
+export { removeCalloutByKey } from './helpers/remove-callout-by-key/RemoveCalloutByKey';
