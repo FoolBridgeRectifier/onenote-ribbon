5ff3587e82a947370367a2fba6584db2d4381164
Dragus <zathyadark@gmail.com>
2026-05-09 00:45:12 -0500

    test console

--- src/shared/editor-v2/detection-engine/get-active-tags/build-content-tags/buildContentTags.ts
+++ src/shared/editor-v2/detection-engine/get-active-tags/build-content-tags/buildContentTags.ts
@@ -1,6 +1,7 @@
 import type { TMatchRecord } from '../find-all-matches/interfaces';
 import type { TMatchFilter } from './interfaces';
 import { findAllMatches } from '../find-all-matches/findAllMatches';
+import { tagPositionToRange } from '../utils';
 import {
   filterInvalidCodeMatches,
   filterTagsInCodeBlocks,
@@ -19,5 +20,21 @@ export const buildContentTags = (content: string) => {
     filterTagsInHtml,
   ];
 
-  return filters.reduce<TMatchRecord[]>((matches, filter) => filter(content, matches), allMatches);
+  const tags = filters.reduce<TMatchRecord[]>(
+    (matches, filter) => filter(content, matches),
+    allMatches
+  );
+
+  console.warn(
+    '[buildContentTags]',
+    tags.map((tag) => ({
+      type: tag.type,
+      open: tag.open,
+      openText: tag.open && (({ offset, length }) => content.slice(offset, offset + length))(tagPositionToRange(tag.open, content)),
+      close: tag.close,
+      closeText: tag.close && (({ offset, length }) => content.slice(offset, offset + length))(tagPositionToRange(tag.close, content)),
+    }))
+  );
+
+  return tags;
 };
--- src/shared/editor-v2/detection-engine/get-active-tags/getActiveTags.ts
+++ src/shared/editor-v2/detection-engine/get-active-tags/getActiveTags.ts
@@ -2,6 +2,5 @@ import { buildContentTags } from './build-content-tags/buildContentTags';
 
 export const getActiveTags = (content: string, _cursor: unknown) => {
   const tags = buildContentTags(content);
-  console.warn(tags);
   return tags;
 };
